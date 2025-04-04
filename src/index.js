b8ba9048cc64a53705aa421e9e056c93d50e1d9d
SirCega <joancanizales08@gmail.com>
2025-04-04 17:28:01 -0500

    first commit-web server

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+import app from "./app.js";
+
+const main = () =>{
+  app.listen(app.get("port"));
+  console.log(`The company's web server is running on port ${app.get("port")}`);
+  
+
+}
+
+main();
+
